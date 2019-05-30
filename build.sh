1cfa22eea495c214fb2cbb2636ec6bc103017e57
Scott Covert <scott.b.covert@gmail.com>
2019-05-30 04:29:11 -0700

    Removing unnecessary echo statements

--- build.sh
+++ build.sh
@@ -53,19 +53,19 @@ then
 fi
 
 cd "$WSPACE"
-echo Changing directoy to $WSPACE
+#echo Changing directoy to $WSPACE
 rm -rf $BUILDDIR
-echo Delete pre-existing $BUILDDIR directory
+#echo Delete pre-existing $BUILDDIR directory
 mkdir $BUILDDIR
-echo Creating $BUILDDIR directory
+#echo Creating $BUILDDIR directory
 rm -rf $DESTROYDIR
-echo Delete pre-existing $DESTROYDIR directory
+#echo Delete pre-existing $DESTROYDIR directory
 cd $ROOTDIR
-echo Changing directory to $ROOTDIR
+#echo Changing directory to $ROOTDIR
 if [ ! -z "$PRIORCOMMIT" ]
 then
         git diff --diff-filter=d -z --name-only $PRIORCOMMIT $CURRCOMMIT -- . ":!$EXCLDIR" -- . ":!*.*ignore" | xargs -0 -IREPLACE rsync -aR REPLACE "$WSPACE/$BUILDDIR/$ROOTDIR"
-        echo Moving changed files to $BUILDDIR folder
+        #echo Moving changed files to $BUILDDIR folder
         IFS=$'\n';
         for f in $(find "$WSPACE/$BUILDDIR/$SRCDIR" -name '*-meta.xml')
         do
@@ -83,14 +83,14 @@ then
                         cp -pv "$WSPACE/$SRCDIR/$DIR/$BNAME" "$WSPACE/$BUILDDIR/$SRCDIR/$DIR/"
                 fi
         done
-        echo Moving all related components to changed metadata files and all related aura/lwc files within a changed bundle to $BUILDDIR folder
+        #echo Moving all related components to changed metadata files and all related aura/lwc files within a changed bundle to $BUILDDIR folder
         for f in $(find "$WSPACE/$BUILDDIR/$SRCDIR" -name '*.cls' -or -name '*.component' -or -name '*.page' -or -name '*.resource' -or -name '*.trigger' -or -name '*.app' -not -not -path '$SRCDIR/applications/*' -or -name '*.cmp' -or -name '*.design' -or -name '*.evt' -or -name '*.intf' -or -name '*.js' -or -name '*.svg' -or -name '*.css' -or -name '*.auradoc' -or -name '*.tokens' -or -name '*.html' -or -name '*.js-meta.xml' -or -name '*.js-meta.xml')
         do
                 BNAME=`basename $f`
                 BNAME="$BNAME-meta.xml"
                 DIR=`dirname $f`
                 DIR=${DIR##*$SRCDIR/}
-                echo $DIR
+                #echo $DIR
                 if [[ $DIR == "aura/"* ]]
                 then
                         cp -Rpv "$WSPACE/$SRCDIR/$DIR" "$WSPACE/$BUILDDIR/$SRCDIR/aura/"
@@ -101,34 +101,34 @@ then
                         cp -pv "$WSPACE/$SRCDIR/$DIR/$BNAME" "$WSPACE/$BUILDDIR/$SRCDIR/$DIR/"
                 fi
         done
-        echo Moving all metadata files related to deleted components and all related aura/lwc files within a changed bundle to $BUILDDIR folder
+        #echo Moving all metadata files related to deleted components and all related aura/lwc files within a changed bundle to $BUILDDIR folder
         if [ `git diff --diff-filter=D -z --name-only $PRIORCOMMIT $CURRCOMMIT` ]
         then
                 mkdir $DESTROYDIR
-                echo Creating $DESTROYDIR directory
+                #echo Creating $DESTROYDIR directory
                 rm -f deletedFiles.txt
-                echo Deleting pre-existing deletedFiles.txt list
+                #echo Deleting pre-existing deletedFiles.txt list
                 git diff --diff-filter=D -z --name-only $PRIORCOMMIT $CURRCOMMIT | xargs -0 -IDELFILE echo "DELFILE" >> deletedFiles.txt
-                echo Creating new deletedFiles.txt list
+                #echo Creating new deletedFiles.txt list
                 IFS=$'\n';
                 while read CFILE
                 do
-                        echo Analyzing file `basename $CFILE`
+                        #echo Analyzing file `basename $CFILE`
                         BNAME=`basename $CFILE`
                         BNAME="${BNAME//-meta.xml}"
                         DIR=`dirname $CFILE`
                         DIR=${DIR##*$SRCDIR/}
-                        echo Directory is $DIR
+                        #echo Directory is $DIR
                         if [[ -d "$SRCDIR/$DIR" && "$(ls -A $SRCDIR/$DIR)" && $DIR == "aura/"* ]]
                         then
-                                echo "Lightning component metadata has been deleted, but the component bundle still exists with files so the bundle should be incorporated for updating, not total deletion"
+                                #echo "Lightning component metadata has been deleted, but the component bundle still exists with files so the bundle should be incorporated for updating, not total deletion"
                                 mkdir -p "$WSPACE/$BUILDDIR/$SRCDIR/aura/" && cp -Rpv "$WSPACE/$SRCDIR/$DIR" "$WSPACE/$BUILDDIR/$SRCDIR/aura/"
                         elif [[ -d "$SRCDIR/$DIR" && "$(ls -A $SRCDIR/$DIR)" && $DIR == "lwc/"* ]]
                         then
-                                echo "Lightning web component metadata has been deleted, but the component bundle still exists with files so the bundle should be incorporated for updating, not total deletion"
+                                #echo "Lightning web component metadata has been deleted, but the component bundle still exists with files so the bundle should be incorporated for updating, not total deletion"
                                 mkdir -p "$WSPACE/$BUILDDIR/$SRCDIR/lwc/" && cp -Rpv "$WSPACE/$SRCDIR/$DIR" "$WSPACE/$BUILDDIR/$SRCDIR/lwc/"
                         else
-                                echo "Either the metadata deleted is not part of a component bundle or the entire bundle has been deleted"
+                                #echo "Either the metadata deleted is not part of a component bundle or the entire bundle has been deleted"
                                 mkdir -p "$WSPACE/$DESTROYDIR/$SRCDIR/$DIR" && echo "$BNAME is going to be deleted!" > "$WSPACE/$DESTROYDIR/$SRCDIR/$DIR/$BNAME"
                         fi
                 done < deletedFiles.txt
@@ -142,16 +142,16 @@ then
 <Package>
 </Package>
 EOF
-        echo ===PKGXML===
-        echo $NEWPKGXML
-        echo Creating new package.xml
-        echo $NEWPKGXML > "$WSPACE/$BUILDDIR/$SRCDIR/package.xml"
+        #echo ===PKGXML===
+        #echo $NEWPKGXML
+        #echo Creating new package.xml
+        #echo $NEWPKGXML > "$WSPACE/$BUILDDIR/$SRCDIR/package.xml"
 
         declare -a elementArray
         IFS=$'\n'
         for CFILE in $(find "$WSPACE/$BUILDDIR")
         do
-                echo Analyzing file `basename $CFILE`
+                #echo Analyzing file `basename $CFILE`
 
                 case "$CFILE"
                 in
@@ -189,37 +189,37 @@ EOF
                         ENTITY=$(basename "$CFILE")
                         ENTITY="${ENTITY%.*}"
                         ENTITY="${ENTITY//.*-meta*}"
-                        echo ENTITY NAME: $ENTITY
+                        #echo ENTITY NAME: $ENTITY
 
                         if grep -Fq "$TYPENAME" "$WSPACE/$BUILDDIR/$SRCDIR/package.xml"
                         then
                                 if [[ ! " ${elementArray[@]} " =~ " $TYPENAME$ENTITY " ]]
                                 then
-                                        echo Generating new member for $ENTITY
+                                        #echo Generating new member for $ENTITY
                                         xmlstarlet ed -L -s "/Package/types[name='$TYPENAME']" -t elem -n members -v "$ENTITY" "$WSPACE/$BUILDDIR/$SRCDIR/package.xml"
                                         elementArray+=($TYPENAME$ENTITY)
                                 else
                                         echo Skipping to avoid duplicate entry in package.xml
                                 fi
                         else
-                                echo Generating new $TYPENAME type
+                                #echo Generating new $TYPENAME type
                                 xmlstarlet ed -L -s /Package -t elem -n types -v "" "$WSPACE/$BUILDDIR/$SRCDIR/package.xml"
                                 xmlstarlet ed -L -s '/Package/types[not(*)]' -t elem -n name -v "$TYPENAME" "$WSPACE/$BUILDDIR/$SRCDIR/package.xml"
-                                echo Generating new member for $ENTITY
+                                #echo Generating new member for $ENTITY
                                 xmlstarlet ed -L -s "/Package/types[name='$TYPENAME']" -t elem -n members -v "$ENTITY" "$WSPACE/$BUILDDIR/$SRCDIR/package.xml"
                                 elementArray+=($TYPENAME$ENTITY)
                         fi
                 else
                         echo ERROR: UNKNOWN FILE TYPE $CFILE
                 fi
-                echo ====UPDATED PACKAGE.XML====
+                #echo ====UPDATED PACKAGE.XML====
                 cat "$WSPACE/$BUILDDIR/$SRCDIR/package.xml"
         done
-        echo Cleaning up Package.xml
+        #echo Cleaning up Package.xml
         xmlstarlet ed -L -s /Package -t elem -n version -v "45.0" "$WSPACE/$BUILDDIR/$SRCDIR/package.xml"
         xmlstarlet ed -L -i /Package -t attr -n xmlns -v "http://soap.sforce.com/2006/04/metadata" "$WSPACE/$BUILDDIR/$SRCDIR/package.xml"
 
-        echo ====FINAL PACKAGE.XML=====
+        #echo ====FINAL PACKAGE.XML=====
         cat "$WSPACE/$BUILDDIR/$SRCDIR/package.xml"
         fi
         if [ -d "$WSPACE/$DESTROYDIR" ]
@@ -229,15 +229,15 @@ EOF
 <Package>
 </Package>
 EOF
-                echo ===DESTROYXML===
-                echo $NEWDESTROYXML
-                echo Creating new destructiveChangesPost.xml
-                echo $NEWDESTROYXML > "$WSPACE/$BUILDDIR/$SRCDIR/destructiveChangesPost.xml"
+                #echo ===DESTROYXML===
+                #echo $NEWDESTROYXML
+                #echo Creating new destructiveChangesPost.xml
+                #echo $NEWDESTROYXML > "$WSPACE/$BUILDDIR/$SRCDIR/destructiveChangesPost.xml"
                 declare -a destroyElementArray
                 IFS=$'\n'
                 for CFILE in $(find "$WSPACE/$DESTROYDIR")
                 do
-                        echo Analyzing file `basename $CFILE`
+                        #echo Analyzing file `basename $CFILE`
 
                         case "$CFILE"
                         in
@@ -273,43 +273,43 @@ EOF
                                 ENTITY=$(basename "$CFILE")
                                 ENTITY="${ENTITY%.*}"
                                 ENTITY="${ENTITY//.*-meta*}"
-                                echo ENTITY NAME: $ENTITY
+                                #echo ENTITY NAME: $ENTITY
 
                                 if grep -Fq "$TYPENAME" "$WSPACE/$BUILDDIR/$SRCDIR/destructiveChangesPost.xml"
                                 then
                                         if [[ ! " ${destroyElementArray[@]} " =~ " $TYPENAME$ENTITY " ]]
                                         then
-                                                echo Generating new member for $ENTITY
+                                                #echo Generating new member for $ENTITY
                                                 xmlstarlet ed -L -s "/Package/types[name='$TYPENAME']" -t elem -n members -v "$ENTITY" "$WSPACE/$BUILDDIR/$SRCDIR/destructiveChangesPost.xml"
                                                 destroyElementArray+=($TYPENAME$ENTITY)
                                         else
                                                 echo Skipping to avoid duplicate entry in destructiveChangesPost.xml
                                         fi
                                 else
-                                        echo Generating new $TYPENAME type
+                                        #echo Generating new $TYPENAME type
                                         xmlstarlet ed -L -s /Package -t elem -n types -v "" "$WSPACE/$BUILDDIR/$SRCDIR/destructiveChangesPost.xml"
                                         xmlstarlet ed -L -s '/Package/types[not(*)]' -t elem -n name -v "$TYPENAME" "$WSPACE/$BUILDDIR/$SRCDIR/destructiveChangesPost.xml"
-                                        echo Generating new member for $ENTITY
+                                        #echo Generating new member for $ENTITY
                                         xmlstarlet ed -L -s "/Package/types[name='$TYPENAME']" -t elem -n members -v "$ENTITY" "$WSPACE/$BUILDDIR/$SRCDIR/destructiveChangesPost.xml"
                                         destroyElementArray+=($TYPENAME$ENTITY)
                                 fi
                         else
                                 echo ERROR: UNKNOWN FILE TYPE $CFILE
                         fi
-                        echo ====UPDATED destructiveChangesPost.xml====
+                        #echo ====UPDATED destructiveChangesPost.xml====
                         cat "$WSPACE/$BUILDDIR/$SRCDIR/destructiveChangesPost.xml"
                 done
-                echo Cleaning up destructiveChangesPost.xml
+                #echo Cleaning up destructiveChangesPost.xml
                 xmlstarlet ed -L -s /Package -t elem -n version -v "45.0" "$WSPACE/$BUILDDIR/$SRCDIR/destructiveChangesPost.xml"
                 xmlstarlet ed -L -i /Package -t attr -n xmlns -v "http://soap.sforce.com/2006/04/metadata" "$WSPACE/$BUILDDIR/$SRCDIR/destructiveChangesPost.xml"
 
-                echo ====FINAL destructiveChangesPost.xml=====
+                #echo ====FINAL destructiveChangesPost.xml=====
                 cat "$WSPACE/$BUILDDIR/$SRCDIR/destructiveChangesPost.xml"
         fi
 else
-        echo No prior commit found, default Package.xml will be used
+        #echo No prior commit found, default Package.xml will be used
         rsync -aRv --exclude="$SRCDIR/*.*ignore" --exclude="$SRCDIR/**/*.*ignore" --exclude="$SRCDIR/.git/" --exclude="$SRCDIR/**/.git/" --exclude="$SRCDIR/$EXCLDIR/" --exclude="$SRCDIR/**/$EXCLDIR/" $SRCDIR $BUILDDIR/
-        echo Moving files to $BUILDDIR folder
+        #echo Moving files to $BUILDDIR folder
 fi
-echo Return to workspace directory
+#echo Return to workspace directory
 cd "$WSPACE"
